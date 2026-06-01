c53c1ea948dc2d5dcbf3752cb9462785d10a9a61
John Paul Barona <mannbaldeo@gmail.com>
2026-06-02 01:14:45 +0800

    Update Firebase config to use environment variables

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,21 +1,12 @@
 // backend/src/config/firebase.js
 const admin = require('firebase-admin');
 
-// ✨ We changed the name so the rogue .env file cannot override us!
-const base64Credentials = process.env.VERCEL_FIREBASE_BASE64;
+// Go up two folders (../../) to find the key in the root backend folder
+const serviceAccount = require('../../firebase-service-account.json');
 
-if (!base64Credentials) {
-  console.error("CRITICAL ERROR: Missing VERCEL_FIREBASE_BASE64 variable!");
-}
-
-const decodedCredentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
-const serviceAccount = JSON.parse(decodedCredentials);
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const db = admin.firestore();
 
